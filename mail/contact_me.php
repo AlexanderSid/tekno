0c608b327ba2d1f2aa000d68ceef1f2b42d84c95
AlexanderSid <alexandersidum@gmail.com>
2018-03-14 20:37:13 +0700

    Update contact_me.php

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -15,11 +15,11 @@ $email_address = strip_tags(htmlspecialchars($_POST['email']));
 $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
    
-// Create the email and send the message
-$to = 'alexandersidum@gmail.com'; // tujuan
+
+$to = 'admin@tekinfoits.tk'; // tujuan
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@teknoinfo.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers = "From: admin@tekinfoits.tk\n"; // asal
 $headers .= "Reply-To: $email_address";   
 mail($to,$email_subject,$email_body,$headers);
 return true;         
