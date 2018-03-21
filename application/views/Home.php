3315c29c970f8fe4753f25386e5c585d07e8d809
AlexanderSid <alexandersidum@gmail.com>
2018-03-21 20:22:51 +0700

    Add files via upload

---
+++ application/views/Home.php
@@ -0,0 +1,302 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Teknologi Informasi</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom fonts for this template -->
+    <link href="assets/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
+
+    <!-- Custom styles for this template -->
+    <link href="assets/css/agency.min.css" rel="stylesheet">
+
+  </head>
+
+  <body id="page-top">
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
+      <div class="container">
+        <a class="navbar-brand js-scroll-trigger" href="#page-top">Teknologi Informasi</a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          Menu
+          <i class="fa fa-bars"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav text-uppercase ml-auto">
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#services">Profil Lulusan</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#portfolio">Fasilitas</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#about">Visi&Misi</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#team">Kata Mereka</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#contact">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <!-- Header -->
+    <header class="masthead">
+      <div class="container">
+        <div class="intro-text">
+          <div class="intro-lead-in">Fasilitas Teknologi Informasi dan Komunikasi</div>
+          <div class="intro-heading text-uppercase">TEKNOLOGI INFORMASI</div>
+          <a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="#services">Detail</a>
+        </div>
+      </div>
+    </header>
+
+    <!-- Profil Lulusan -->
+    <section id="services">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12 text-center">
+            <h2 class="section-heading text-uppercase">Profil Lulusan</h2>
+            <h3 class="section-subheading text-muted">Beberapa dari banyak profesi yang dapat ditempuh lulusan.</h3>
+            <p class="text-muted"></p>
+          </div>
+        </div>
+        <div class="row text-center">
+          <div class="col-md-4">
+            <span class="fa-stack fa-4x">
+              <i class="fa fa-circle fa-stack-2x text-primary"></i>
+              <i class="fa fa-lock fa-stack-1x fa-inverse"></i>
+            </span>
+            <h4 class="service-heading">Cyber Security Specialist</h4>
+            <p class="text-muted">Spesialis menjaga keamanan Cyber</p>
+          </div>
+          <div class="col-md-4">
+            <span class="fa-stack fa-4x">
+              <i class="fa fa-circle fa-stack-2x text-primary"></i>
+              <i class="fa fa-laptop fa-stack-1x fa-inverse"></i>
+            </span>
+            <h4 class="service-heading">IOT Specialist</h4>
+            <p class="text-muted"> Internet of Things Spesialis.</p>
+          </div>
+          <div class="col-md-4">
+            <span class="fa-stack fa-4x">
+              <i class="fa fa-circle fa-stack-2x text-primary"></i>
+              <i class="fa fa-lock fa-stack-1x fa-inverse"></i>
+            </span>
+            <h4 class="service-heading">Application Security Analyst</h4>
+            <p class="text-muted">Analis keamanan Aplikasi.</p>
+          </div>
+          <div class="col-md-4">
+            <span class="fa-stack fa-4x">
+              <i class="fa fa-circle fa-stack-2x text-primary"></i>
+              <i class="fa fa-cloud fa-stack-1x fa-inverse"></i>
+            </span>
+            <h4 class="service-heading">Cloud Service Developer</h4>
+            <p class="text-muted">Pengembang dan penyedia jasa Cloud.</p>
+          </div>
+           <div class="col-md-4">
+            <span class="fa-stack fa-4x">
+              <i class="fa fa-circle fa-stack-2x text-primary"></i>
+              <i class="fa fa-cog fa-stack-1x fa-inverse"></i>
+            </span>
+            <h4 class="service-heading">System Integration Specialist</h4>
+            <p class="text-muted">Spesialis integrasi sistem.</p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- Fasilitas -->
+    <section class="bg-light" id="portfolio">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12 text-center">
+            <h2 class="section-heading text-uppercase">Fasilitas</h2>
+            <h3 class="section-subheading text-muted">Sejumlah fasilitas yang disediakan.</h3>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-4 col-sm-6 portfolio-item">
+         
+              <img class="img-fluid" src="assets/img/portfolio/01-thumbnail.jpg" alt="">
+           
+            <div class="portfolio-caption">
+              <h4>Laboratorium</h4>
+             
+            </div>
+          </div>
+          <div class="col-md-4 col-sm-6 portfolio-item">
+           
+              <img class="img-fluid" src="assets/img/portfolio/02-thumbnail.jpg" alt="">
+           
+            <div class="portfolio-caption">
+              <h4>Ruang Baca</h4>
+              
+            </div>
+          </div>
+          <div class="col-md-4 col-sm-6 portfolio-item">
+            
+              <img class="img-fluid" src="assets/img/portfolio/03-thumbnail.jpg" alt="">
+           
+            <div class="portfolio-caption">
+              <h4>Ruang Kelas</h4>
+              
+            </div>
+          </div>
+         
+        </div>
+      </div>
+    </section>
+
+    <!-- Visi Misi -->
+    <section id="about">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12 text-center">
+            <h2 class="section-heading text-uppercase">Visi & Misi</h2>
+          </div>
+        </div>
+        <div class="row">
+          <h3 > Visi  </h3>
+          <p>Menjadi Program Studi Teknologi Informasi yang unggul dalam bidang keamanan siber dan Teknologi berbasis Internet (Internet of Things) untuk mendukung pembangunan Smart City secara berkelanjutan hingga tahun 2022. </p>
+          <h3> Misi </h3>
+          <p>1. Menyelenggarakan pendidikan dan pengajaran Teknologi Informasi dengan menggunakan kurikulum yang adaptif. berorientas ke masa depan dan didukung SDM yang berkualitas serta fasilitas yang memadai. </p>
+          <p>2. Melaksanakan penelitian yangbermutu dibidang Keamanan Siber dan Internet of Things untuk teknologi Smart City. </p>
+          <p>3. Menjalin kemitraan dengan instansi dalam maupun luar negeri. </p>
+          <p>4. Menyelenggarakan pengabdian kepada masyarakat berupa pelatihan, penyuluhan, penerapan hasil penelitian untuk pengembanganpotensidanpemberdayaanmasyarakat daerah.</p>
+ 
+
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- Kata Mereka -->
+    <section class="bg-light" id="team">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12 text-center">
+            <h2 class="section-heading text-uppercase">Apa Kata Mereka?</h2>
+            <h3 class="section-subheading text-muted">...</h3>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-sm-4">
+            <div class="team-member">
+              <img class="mx-auto rounded-circle" src="assets/img/team/1.jpg" alt="">
+              <h4>Bill Gates</h4>
+              <p class="text-muted">Microsoft</p>
+              <p class="text-muted">"Jika kita tidak memecahkan masalah keamanan, maka orang - orang akan ragu"</p>
+            </div>
+          </div>
+          <div class="col-sm-4">
+            <div class="team-member">
+              <img class="mx-auto rounded-circle" src="assets/img/team/2.jpg" alt="">
+              <h4>Wiranto</h4>
+              <p class="text-muted">Menkopolhukam</p>
+              <p class="text-muted">"Kegiatan siber nasional terutama pengamanan siber ini merupaka keharusan, keniscayaan.."</p>
+            </div>
+          </div>
+          <div class="col-sm-4">
+            <div class="team-member">
+              <img class="mx-auto rounded-circle" src="assets/img/team/3.jpg" alt="">
+              <h4>Rudiantara</h4>
+              <p class="text-muted">Menkominfo</p>
+              <p class="text-muted">"SMART City menciptakan perubahan sistem lebih efektif dan efisien dalam lembaga pemerintahan"</p>
+            </div>
+          </div>
+          
+        </div>
+        
+        
+        <div class="row">
+          <div class="col-lg-8 mx-auto text-center">
+            <p class="large text-muted">...</p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+   
+
+    <!-- Contact -->
+      <section id="contact">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12 text-center">
+            <h2 class="section-heading text-uppercase">Contact Us</h2>
+            <h3 class="section-subheading text-muted">...</h3>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-lg-12">
+            <form id="contactForm" name="sentMessage" novalidate>
+              <div class="row">
+                <div class="col-md-6">
+                  <div class="form-group">
+                    <input class="form-control" id="name" type="text" placeholder="Masukkan nama" required data-validation-required-message="Please enter your name.">
+                    <p class="help-block text-danger"></p>
+                  </div>
+                  <div class="form-group">
+                    <input class="form-control" id="email" type="email" placeholder="Masukkan email*" required data-validation-required-message="Please enter your email address.">
+                    <p class="help-block text-danger"></p>
+                  </div>
+                  <div class="form-group">
+                    <input class="form-control" id="phone" type="tel" placeholder="Masukkan no. hp" required data-validation-required-message="Please enter your phone number.">
+                    <p class="help-block text-danger"></p>
+                  </div>
+                </div>
+                <div class="col-md-6">
+                  <div class="form-group">
+                    <textarea class="form-control" id="message" placeholder="Tulis pesan anda disini" required data-validation-required-message="Please enter a message."></textarea>
+                    <p class="help-block text-danger"></p>
+                  </div>
+                </div>
+                <div class="clearfix"></div>
+                <div class="col-lg-12 text-center">
+                  <div id="success"></div>
+                  <button id="sendMessageButton" class="btn btn-primary btn-xl text-uppercase" type="submit">Send Message</button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="assets/vendor/jquery/jquery.min.js"></script>
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="assets/vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Contact form JavaScript -->
+    <script src="assets/js/jqBootstrapValidation.js"></script>
+    <script src="assets/js/contact_me.js"></script>
+
+    <!-- Custom scripts for this template -->
+    <script src="assets/js/agency.min.js"></script>
+
+  </body>
+
+</html>
